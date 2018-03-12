edb8dc06edc3a6f6f6474f9c3bb467cc2858fa99
Gokul <gokul@ajirasoft.com>
2018-03-12 17:14:18 +0530

    enables app to listen to and connect to perrs

---
+++ skeleton/part5/main.go
@@ -0,0 +1,106 @@
+// Solution to part 5 of the Whispering Gophers code lab.
+//
+// This program extends part 4.
+//
+// It listens on an available public IP and port, and for each incoming
+// connection it decodes JSON-encoded messages and writes them to standard
+// output.
+// It simultaneously makes a connection to the host and port specified by -peer,
+// reads lines from standard input, and writes JSON-encoded messages to the
+// network connection.
+// The messages include the listen address. For example:
+// 	{"Addr": "127.0.0.1:41232", "Body": "Hello"!}
+//
+// You can test this program by listening with the dump program:
+// 	$ dump -listen=localhost:8000
+// In another terminal, running this program:
+// 	$ part5 -peer=localhost:8000
+// And in a third terminal, running part2 with the address printed by part5:
+// 	$ part2 -dial=192.168.1.200:54312
+// Lines typed in the third terminal should appear in the second, and those
+// typed in the second window should appear in the first.
+//
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	util2 "github.com/gokul/whispering-gophers/util"
+)
+
+var (
+	peerAddr = flag.String("peer", "", "peer host:port")
+	self     string
+)
+
+type Message struct {
+	Addr string
+	Body string
+}
+
+func main() {
+	flag.Parse()
+
+	l, err := util2.Listen()
+
+	self = l.Addr().String()
+
+	fmt.Print(self)
+
+	if err != nil {
+		log.Fatal(l)
+	}
+
+	go dial(*peerAddr)
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go serve(c)
+	}
+}
+
+func serve(c net.Conn) {
+	defer c.Close()
+	d := json.NewDecoder(c)
+	for {
+		var m Message
+		err := d.Decode(&m)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Printf("%#v\n", m)
+	}
+}
+
+func dial(addr string) {
+	c, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := bufio.NewScanner(os.Stdin)
+	e := json.NewEncoder(c)
+	for s.Scan() {
+		m := Message{
+			Addr: addr,
+			Body: s.Text(),
+		}
+		err := e.Encode(m)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
